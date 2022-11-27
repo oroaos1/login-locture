596878e54ec6e4dc6cf8604fe996737caf4ecd11
ParkBomJun <rkrsion@naver.com>
2022-11-27 16:05:40 +0900

    깃 저장소 초기화

---
+++ routes/home/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const Router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+Router.get("/", ctrl.hello);
+
+Router.get("/login", ctrl.login);
+
+module.exports = Router;
